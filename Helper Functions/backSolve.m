2f642cf8cf3079ebc89bbd138573fb638004a656
pcoones <paulcoones@hotmail.com>
2026-06-28 22:17:45 -0400

    Fixed comments and notation A-->U

--- Helper Functions/backSolve.m
+++ Helper Functions/backSolve.m
@@ -1,19 +1,19 @@
 %{ 
 backSolve.m provides a standalone function for computing the solution
-to Ax = b, using the backward solve algorithm.
-Unnecessary to luGelim.m but provided for learning purposes.
+to Ux = b provided a upper triangular matrix U, through the backward solve 
+algorithm.
 %}
 
-function x = backSolve(A, b) 
-% x = backSolve(A, b) Returns the solution to the matrix equation Ax = b
-% Requires: A is a square upper-triangular matrix
-%           A is non-zero along any diagonal elements
+function x = backSolve(U, b) 
+% x = backSolve(U, b) Returns the solution to the matrix equation Ux = b
+% Requires: U is a square upper-triangular matrix
+%           U is non-zero along any diagonal elements
 % Runtime: O(n^2)
 n = length(b);
 x = b; % Why set x = b, we continually modify x to do the iteration                
-for row = n:-1:1 % Recall we are solving backwards for a_nn x_n = b_n first!
+for row = n:-1:1 % Recall we are solving backwards for u_nn x_n = b_n first!
     for col = row+1:1:n
-        x(row) = x(row) - A(row,col)*x(col);
+        x(row) = x(row) - U(row,col)*x(col);
     end
-    x(row) = x(row) / A(row, row); % Avoids (n - row) unnecessary divisions
+    x(row) = x(row) / U(row, row); % Avoids (n - row) unnecessary divisions
 end
